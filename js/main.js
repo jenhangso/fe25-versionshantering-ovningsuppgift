0f4644cf71d8329fd3ca5eb38a22d149573f1b54
Jenny Johansen <src97vyzwn@privaterelay.appleid.com>
2026-03-11 11:57:53 +0100

    mer styling & små fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-import { displayAstronomyPicture } from "./moduler/displayFunctions.js";
-import { getAstronomyPicture } from "./moduler/apiFunctions.js";
+// import { displayAstronomyPicture } from "./moduler/displayFunctions.js";
+// import { getAstronomyPicture } from "./moduler/apiFunctions.js";
 
 const menuBtn = document.querySelector('#mobile-menu');
 const navList = document.querySelector('#nav-links');
@@ -8,8 +8,8 @@ menuBtn.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
 
-getAstronomyPicture()
-  .then( displayAstronomyPicture )
-  .catch( error => {
-        displayError(error);
-    });
+// getAstronomyPicture()
+//   .then( displayAstronomyPicture )
+//   .catch( error => {
+//         displayError(error);
+//     });
