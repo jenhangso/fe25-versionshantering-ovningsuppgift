2496783c8e6903bf91db992e07d1cda415bd0437
Emma Persson <emma.persson1@gritacademy.se>
2026-03-16 12:55:44 +0100

    changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-<<<<<<< Updated upstream
-import { fetchAPOD } from "./moduler/apiFunctions.js";
-import { displayAFOD } from "./moduler/displayFunctions.js";
-=======
 import { displayAstronomyPicture } from "./moduler/displayFunctions.js";
 import { getAstronomyPicture } from "./moduler/apiFunctions.js";
->>>>>>> Stashed changes
 
 const menuBtn = document.querySelector('#mobile-menu');
 const navList = document.querySelector('#nav-links');
@@ -17,19 +12,8 @@ menuBtn.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
 
-<<<<<<< Updated upstream
-(async () => {
-    fetchAPOD().then(apodData => {
-        displayAFOD(apodData);
-    }).catch(error => {
-        apodInfoTitle.textContent = 'Error fetching data';
-        apodInfoExplenation.textContent = error.message;
-    });
-})();
-=======
 getAstronomyPicture()
   .then( displayAstronomyPicture )
   .catch( error => {
         displayError(error);
     });
->>>>>>> Stashed changes
