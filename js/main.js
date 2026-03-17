a0ff15fe33a7f803b430bfeaf6f444f60992caf4
Emma Persson <emma.persson1@gritacademy.se>
2026-03-17 09:39:02 +0100

    Refactor HTML structure and improve CSS styles; update JavaScript for better functionality

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-import { displayAPOD, displaySpaceNews } from './moduler/displayFunctions.js';
+import { displayAPOD, displaySpaceNews } from "./moduler/displayFunctions.js";
 
-const menuBtn = document.querySelector('#mobile-menu');
-const navList = document.querySelector('#nav-links');
+const menuBtn = document.querySelector("#mobile-menu");
+const navList = document.querySelector("#nav-links");
 
-menuBtn.addEventListener('click', () => {
-    navList.classList.toggle('active');
+menuBtn.addEventListener("click", () => {
+  navList.classList.toggle("active");
 });
 
 displayAPOD();
